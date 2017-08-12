5b20e2306df09fd92409348e51046cfa9a0e1e1e
Iroleh Vincent <irolehvincent@yahoo.com>
2017-08-12 04:26:12 +0100

    Added view and template for detailed blog post as well as css for the site.

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,8 @@
 from django.db import	models
 from django.utils import timezone
-class	Post(models.Model):
 
+
+class	Post(models.Model):
 	author	=	models.ForeignKey('auth.User')
 	title	=	models.CharField(max_length=200)
 	text	=	models.TextField()
