092c779a8aa18a331b8c2b526e58844c7ffb8c95
Iroleh Vincent <irolehvincent@yahoo.com>
2017-08-19 08:05:09 +0100

    Added views to create/edit blog post inside the site.

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,6 @@
-from	django	import	forms
-from	.models	import	Post
+from django	import	forms
+from .models	import	Post
 class	PostForm(forms.ModelForm):
-				class	Meta:
-								model	=	Post
-								fields	=	('title',	'text',)
+	class	Meta:
+		model	=	Post
+		fields	=	('title',	'text',)
